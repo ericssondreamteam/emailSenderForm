00e5d1e6dbb73857162f1a2aa649ba8b5303a190
emaatrb <Mathi018>
2019-08-06 10:29:18 +0200

    "taki teścik"

---
+++ src/main/java/com/jcg/spring/mvc/email/Mail.java
@@ -0,0 +1,29 @@
+package com.jcg.spring.mvc.email;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.mail.MailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
+
+import java.util.Properties;
+
+public class Mail {
+    @Bean
+    MailSender mailSender() {
+        JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
+
+        mailSender.setHost("smtp.office365.com");
+        mailSender.setPort(587);
+
+        mailSender.setUsername("ericssonstart@outlook.com");
+        mailSender.setPassword("qwerty12345");
+
+        Properties props = mailSender.getJavaMailProperties();
+        props.put("mail.transport.protocol", "smtp");
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.starttls.enable", "true");
+        props.put("mail.smtp.quitwait", "false");
+        mailSender.setJavaMailProperties(props);
+
+        return mailSender;
+    }
+}
